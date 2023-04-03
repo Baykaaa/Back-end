6cc663af063dacd132c192849f80fd494713c0b9
Baykaa <bayrjargal.d26@tomujin.edu.mn>
2023-04-03 19:25:15 +0800

    second

--- src/sign.ts
+++ src/sign.ts
@@ -1,42 +1,34 @@
-
-import bcrypt from 'bcrypt';
-import { Pool } from 'pg';
-
+import express from "express";
+import bcrypt from "bcrypt";
+import { Pool } from "pg";
 
 const pool = new Pool({
-  user: 'rtd',
-  host: '54.169.69.87',
-  database: 'pe8db',
-  password: 'Tiny722$',
+  user: "rtd",
+  host: "54.169.69.87",
+  database: "pe8db",
+  password: "Tiny722$",
   port: 3306,
 });
 
+const app = express();
 
-async function signIn(username: string, password: string): Promise<boolean> {
-  
-  const result = await pool.query('SELECT password FROM users WHERE username = $1', [username]);
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const result = await pool.query(
+    "SELECT password FROM users WHERE username = $1",
+    [username]
+  );
   if (result.rowCount === 0) {
-    
-    return false;
+    res.status(401).json({ message: "Incorrect username or password" });
+    return;
   }
+  const dbHashedPassword = result.rows[0].password;
 
-  
-  const hashedPassword = result.rows[0].password;
-
-  
-  const match = await bcrypt.compare(password, hashedPassword);
-  return match;
-}
-
-
-async function createUser(username: string, password: string): Promise<void> {
-  
-  const saltRounds = 10;
-  const salt = await bcrypt.genSalt(saltRounds);
-
- 
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  
-  await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
-}
+  const match = await bcrypt.compare(password, dbHashedPassword);
+  if (match) {
+    res.json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ message: "Incorrect username or password" });
+  }
+});
